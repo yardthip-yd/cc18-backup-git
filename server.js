f9846b5e5271d78868f6ac2f23cb21f2b0f15969
papinwit-l <pae.papinwit@gmail.com>
2024-10-28 15:59:12 +0700

    pull dev

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const authRoute = require("./routes/authRoute")
-const app = express()
+const express = require("express");
+const authRoute = require("./routes/authRoute");
+const app = express();
 
-app.use("/auth", authRoute)
+app.use("/auth", authRoute);
 
-const port = process.env.PORT || 8000
-app.listen(port, () => console.log("Server on", port))
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("Server on", port));
