7d534af6951b338f0ab867c202e48a266b3a488d
chonlathanme <chlmethanan@gmail.com>
2024-10-28 15:49:33 +0700

    Admin Route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const authRoute = require("./routes/authRoute")
+const adminRoute = require("./routes/adminRoute")
 const app = express()
 
 app.use("/auth", authRoute)
+app.use("/admin", adminRoute)
 
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log("Server on", port))
